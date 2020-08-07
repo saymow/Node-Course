aa567b4be2e68e74e31b312a90d5730feed869a0
Gustavo <gustavo.4lv5s@gmail.com>
2020-08-07 08:53:33 -0300

    fix: added description to auth erros

--- task-manager/src/middleware/auth.ts
+++ task-manager/src/middleware/auth.ts
@@ -24,7 +24,7 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
 
     next();
   } catch (error) {
-    res.status(401).send();
+    res.status(401).send(error);
   }
 };
 
